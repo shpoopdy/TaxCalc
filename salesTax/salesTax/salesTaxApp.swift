52270cc427f4c6d7dc35e388986445d6b075e948
Mike <shpoopdy@gmail.com>
2023-05-08 18:25:20 -0700

    Initial implementation

---
+++ salesTax/salesTax/salesTaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  salesTaxApp.swift
+//  salesTax
+//
+//  Created by Mikey Crispin on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct salesTaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
